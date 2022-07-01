7cfc754ceffb162dff190b89c33b5c0660baab4d
懒虫 <11534516+lanchc@users.noreply.github.com>
2022-07-01 14:01:56 +0800

    测试组件

---
+++ Sources/GkAsks/GkAsks.swift
@@ -0,0 +1,8 @@
+
+
+public struct GkAsks {
+    public private(set) var text = "网络模块"
+
+    public init() {
+    }
+}
---
+++ Tests/GkAsksTests/GkAsksTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import GkAsks
+
+final class GkAsksTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(GkAsks().text, "Hello, World!")
+    }
+}
